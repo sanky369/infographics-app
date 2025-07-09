d6bd52030703560dd18a0ab7cffb896904182270
artem-lazarev <artem.lazarev@me.com>
2025-07-09 17:02:10 +0300

    convex set up with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
+import { ClerkProvider } from '@clerk/nextjs'
+import ConvexClientProvider from '@/components/ConvexClientProvider'
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -34,7 +38,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <ClerkProvider>
+            <ConvexClientProvider>
+              {children}
+            </ConvexClientProvider>
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
